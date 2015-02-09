a2f3d264ac16ff12c83827bcd8eb6431f442c60f
Savannah Giese <gieses@onid.oregonstate.edu>
2015-02-08 21:26:07 -0800

    Worked on login page button/username field

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login</title>
+  <meta charset="UTF-8">
+</head>
+
+<body>
+
+<form action="http://savvyg.me/CS_290/assignment4-part1/content1.php" method="post">
+    <p>Username:
+      <input type="text" name="username">
+      <input type="submit" onClick="parent.location='http://savvyg.me/CS_290/assignment4-part1/content1.php'" value='login'>
+    </p>
+</form>
+</body>
+</html>
